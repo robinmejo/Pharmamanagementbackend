8d96e2543946af5e17133a0a6d001b8a6b7c74a5
Robin Mejo <robinmejo19@gmail.com>
2023-04-02 13:00:48 +0530

    1

---
+++ src/main/java/com/inn/cafe/constents/CafeConstants.java
@@ -0,0 +1,13 @@
+package com.inn.cafe.constents;
+
+public class CafeConstants {
+
+    public static final String SOMETHING_WENT_WRONG = "Something Went Wrong.";
+
+    public static final String INVALID_DATA = "Invalid Data.";
+
+    public static final String UNAUTHORIZED_ACCESS = "Unauthorized access.";
+
+    public static final String STORE_LOCATION = "C:\\Users\\91979\\Desktop\\Bill";
+
+}
